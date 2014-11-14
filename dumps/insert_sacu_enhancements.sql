24ecf6f1e1d32772311bc83114a504b4db5af46d
Burke Lougheed <lougheedburke@gmail.com>
2014-11-14 10:02:12 -0800

    sql script for sacu enhancements

---
+++ dumps/insert_sacu_enhancements.sql
@@ -0,0 +1,42 @@
+-- cybran sacu
+insert into enhancements values
+	('45', 'url0301', 'LCH', 'EMP Burst', '1000', '60000', NULL, NULL, 'cs_emp', '3360', 'Description'),
+	('46', 'url0301', 'LCH', 'Rapid Fabricator', '800', '50000', NULL, NULL, 'cs_rapid', '4200', 'Description'),
+	('47', 'url0301', 'Back', 'Nanite Missile System', '800', '30000', NULL, NULL, 'cs_sam', '3360', 'Description'),
+	('48', 'url0301', 'Back', 'Personal Stealth Generator', '600', '18000', '-100', NULL, 'c_stealth', '2240', 'Description'),
+	('49', 'url0301', 'Back', 'Personal Cloaking Generator', '5000', '500000', '-3500', NULL, 'c_cloak', '14000', 'Description'),
+	('50', 'url0301', 'Back', 'Nano-Repair System', '2000', '105000', NULL, NULL, 'cs_nano', '6720', 'Description'),
+	('51', 'url0301', 'RCH', 'Disintegrator Amplifier', '800', '24000', NULL, NULL, 'cs_dmg', '3360', 'Description'),
+	('52', 'url0301', 'RCH', 'Resource Allocation System', '4500', '60000', '1000', '10', 'c_ras', '8400', 'Description');
+
+-- aeon sacu
+INSERT INTO enhancements values
+	('53', 'ual0301', 'LCH', 'Sacrificial System', 150,	2500, null, null, 'as_sacri', 700, 'Description'),
+	('54', 'ual0301', 'LCH', 'Rapid Fabricator', 800, 50000, null, null, 'as_rapid', 4200, 'Description'),
+	('55', 'ual0301', 'Back', 'Personal Shield Generator', 1200, 60000, -300, null, 'a_shield', 5040, 'Description'),
+	('56', 'ual0301', 'Back', 'Heavy Personal Shield Generator', 1500, 100000, -600, null, 'as_ashield', 7000, 'Description'),
+	('57', 'ual0301', 'Back', 'Personal Teleporter', 15000, 1500000, -10000, null, 'a_tele', 21000, 'Description'),
+	('58', 'ual0301', 'Back', 'Nano-Repair System', 1500, 75000, null, null, 'as_nano', 6720, 'Description'),
+	('59', 'ual0301', 'RCH', 'Reaction Refractor', 1250, 36000, null, null, 'as_react', 5040, 'Description'),
+	('60', 'ual0301', 'RCH', 'Resource Allocation System', 4500, 60000, 1000, 10, 'a_ras', 8400, 'Description');
+
+-- uef sacu
+Insert into enhancements values
+	('61', 'uel0301', 'LCH', 'Energy Accelerator', 1000, 45000, null, null, 'us_accel', 5040, 'Description'),
+	('62', 'uel0301', 'LCH', 'Enhanced Sensor System', 1000, 20000, null, null, 'us_sensor', 3360, 'Description'),
+	('63', 'uel0301', 'Back', 'Personal Shield Generator', 2000, 93750, -500, null, 'u_shield', 7000, 'Description'),
+	('64', 'uel0301', 'Back', 'Shield Generator Field', 3500, 400000, -1000, null, 'u_ashield', 11200, 'Description'),
+	('65', 'uel0301', 'Back', 'Radar Jammer', 600, 18000, -100, null, 'us_jammer', 2520, 'Description'),
+	('66', 'uel0301', 'Back', 'C-D2 Engineering Drone', 380, 9000, null, null, 'u_sd2', 2520, 'Description'),
+	('67', 'uel0301', 'RCH', 'Heavy Plasma Refractor', 800, 30000, null, null, 'us_plasma', 3360, 'Description'),
+	('68', 'uel0301', 'RCH', 'Resource Allocation System', 4500, 60000, 1000, 10, 'u_ras', 8400, 'Description');
+
+-- seraphim sacu
+insert into enhancements values
+	('69', 'xsl0301', 'LCH', 'Nano-Repair System', 2500, 75000, null, null, 's_nano', 6720, 'Description'),
+	('70', 'xsl0301', 'LCH', 'Rapid Fabricator', 800, 50000, null, null, 'sa_rapid', 4200, 'Description'),
+	('71', 'xsl0301', 'Back', 'Personal Shield Generator', 1500, 105000, -300, null, 'sa_shield', 6720, 'Description'),
+	('72', 'xsl0301', 'Back', 'Tactical Missile Launcher', 1000, 50000, null, null, 'sa_tact', 4200, 'Description'),
+	('73', 'xsl0301', 'Back', 'Enhanced Sensor System', 800, 36000, null, null, 'sa_sensor', 3360, 'Description'),
+	('74', 'xsl0301', 'RCH', 'Personal Teleporter', 15000, 1500000, -10000, null, 's_tele', 21000, 'Description'),
+	('75', 'xsl0301', 'RCH', 'Overcharge', 4500, 270000, null, null, 'sa_over', 12600, 'Description');
