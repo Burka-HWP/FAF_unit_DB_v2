6e585d1b668e1fbef6b2f0c5566b452e4a69d602
Burke Lougheed <lougheedburke@gmail.com>
2014-11-14 08:10:42 -0800

    updated enhancements partial

--- application/views/_show_enhancements.php
+++ application/views/_show_enhancements.php
@@ -3,114 +3,15 @@
 <br />
 <div class="row-fluid">
     
-    <div class="span4">
-        <h5 class="center">Left Commander Hand</h5>        
-        <br />
-        {LCH}
-        <div class="enhancement-item">
-            <div class="row-fluid">
-                <div class="span2">
-                    <img src="../assets/images/enhancements/{en_icon}.png" />
-                </div>
-                <div class="span10">
-                    <h5><span class="{race}-text">{en_name}</span></h5>
-                </div>
-            </div>
-            <div class="row-fluid">
-                <div class="span12">
-                    <h5 class="attribute">{en_description}</h5>
-                </div>
-            </div>
-            <div class="row-fluid">
-                <div class="span4">
-                    <div class="row-fluid">
-                        <div class="span2">
-                                <img  src="../assets/images/attributes/mass.png" />
-                        </div>
-                        <div class="span10">
-                            <h5>{en_mass_cost}<br />{en_mass_eco}</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="span4">
-                    <div class="row-fluid">
-                        <div class="span2 ">
-                                <img  src="../assets/images/attributes/energy.png" />
-                        </div>
-                        <div class="span10">
-                                <h5>{en_energy_cost}<br />{en_energy_eco}</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="span4">
-                    <div class="row-fluid">
-                        <div class="span2">
-                                <img  src="../assets/images/attributes/time.png" />
-                        </div>
-                        <div class="span10">
-                                <h5>{en_build_time}</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {/LCH}
-    </div>
-    <div class="span4">        
-        <h5 class="center">Commander Back</h5>
-        <br />
-        {Back}
-        <div class="enhancement-item">
-            <div class="row-fluid">
-                <div class="span2">
-                    <img src="../assets/images/enhancements/{en_icon}.png" />
-                </div>
-                <div class="span10">
-                    <h5><span class="{race}-text">{en_name}</span></h5>
-                </div>
-            </div>
-            <div class="row-fluid">
-                <div class="span12">
-                    <h5 class="attribute">{en_description}</h5>
-                </div>
-            </div>
-            <div class="row-fluid">
-                <div class="span4">
-                    <div class="row-fluid">
-                        <div class="span2">
-                                <img  src="../assets/images/attributes/mass.png" />
-                        </div>
-                        <div class="span10">
-                            <h5>{en_mass_cost}<br />{en_mass_eco}</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="span4">
-                    <div class="row-fluid">
-                        <div class="span2 ">
-                                <img  src="../assets/images/attributes/energy.png" />
-                        </div>
-                        <div class="span10">
-                                <h5>{en_energy_cost}<br />{en_energy_eco}</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="span4">
-                    <div class="row-fluid">
-                        <div class="span2">
-                                <img  src="../assets/images/attributes/time.png" />
-                        </div>
-                        <div class="span10">
-                                <h5>{en_build_time}</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {/Back}
-    </div>
     <div class="span4">   
-        <h5 class="center">Right Commander Hand</h5>
+        <div class="row-fluid">
+            <div class="span2">
+                <img src="../assets/images/enhancements/i_rightarm.png" />
+            </div>
+            <div class="span10">
+                <h4>Right Arm</h4>
+            </div>            
+        </div>
         <br />
         {RCH}
         <div class="enhancement-item">
@@ -162,5 +63,126 @@
         </div>
         {/RCH}
     </div>
+    <div class="span4">        
+        <div class="row-fluid">
+            <div class="span2">
+                <img src="../assets/images/enhancements/i_back.png" />
+            </div>
+            <div class="span10">
+                <h4>Back</h4>
+            </div>            
+        </div>
+        <br />
+        {Back}
+        <div class="enhancement-item">
+            <div class="row-fluid">
+                <div class="span2">
+                    <img src="../assets/images/enhancements/{en_icon}.png" />
+                </div>
+                <div class="span10">
+                    <h5><span class="{race}-text">{en_name}</span></h5>
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span12">
+                    <h5 class="attribute">{en_description}</h5>
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span4">
+                    <div class="row-fluid">
+                        <div class="span2">
+                                <img  src="../assets/images/attributes/mass.png" />
+                        </div>
+                        <div class="span10">
+                            <h5>{en_mass_cost}<br />{en_mass_eco}</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="span4">
+                    <div class="row-fluid">
+                        <div class="span2 ">
+                                <img  src="../assets/images/attributes/energy.png" />
+                        </div>
+                        <div class="span10">
+                                <h5>{en_energy_cost}<br />{en_energy_eco}</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="span4">
+                    <div class="row-fluid">
+                        <div class="span2">
+                                <img  src="../assets/images/attributes/time.png" />
+                        </div>
+                        <div class="span10">
+                                <h5>{en_build_time}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/Back}
+    </div>
+    <div class="span4">
+        <div class="row-fluid">
+            <div class="span2">
+                <img src="../assets/images/enhancements/i_leftarm.png" />
+            </div>
+            <div class="span10">
+                <h4>Left Arm</h4>
+            </div>            
+        </div>                
+        <br />
+        {LCH}
+        <div class="enhancement-item">
+            <div class="row-fluid">
+                <div class="span2">
+                    <img src="../assets/images/enhancements/{en_icon}.png" />
+                </div>
+                <div class="span10">
+                    <h5><span class="{race}-text">{en_name}</span></h5>
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span12">
+                    <h5 class="attribute">{en_description}</h5>
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span4">
+                    <div class="row-fluid">
+                        <div class="span2">
+                                <img  src="../assets/images/attributes/mass.png" />
+                        </div>
+                        <div class="span10">
+                            <h5>{en_mass_cost}<br />{en_mass_eco}</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="span4">
+                    <div class="row-fluid">
+                        <div class="span2 ">
+                                <img  src="../assets/images/attributes/energy.png" />
+                        </div>
+                        <div class="span10">
+                                <h5>{en_energy_cost}<br />{en_energy_eco}</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="span4">
+                    <div class="row-fluid">
+                        <div class="span2">
+                                <img  src="../assets/images/attributes/time.png" />
+                        </div>
+                        <div class="span10">
+                                <h5>{en_build_time}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/LCH}
+    </div>
+    
     
 </div>
