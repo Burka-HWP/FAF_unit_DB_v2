40da50935c72389989c8d92eb2cda5e2781fd289
Burke Lougheed <lougheedburke@gmail.com>
2014-11-20 12:30:04 -0800

    collapse/expand added

--- application/views/all.php
+++ application/views/all.php
@@ -15,12 +15,16 @@
             </div>
         </div>
         <div class="columns">
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="command-btn" class="collapse-button" onclick="collapse('command')">Collapse</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Command</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            <div class="row-fluid">
+            <div id="command-data" class="row-fluid">
                 <div class="race-block">                
                     {aeon-command}               
                 </div>
@@ -35,110 +39,159 @@
                 </div>                        
             </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="engineering-btn" class="collapse-button" onclick="expand('engineering')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Engineering</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {engineering-1}
-            {engineering-2}
-            {engineering-3}
+            <div id="engineering-data" style="display: none;">
+                {engineering-1}
+                {engineering-2}
+                {engineering-3}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="factories-btn" class="collapse-button" onclick="expand('factories')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Factories</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {factory-1}
-            {factory-2}
-            {factory-3}
+            <div id="factories-data" style="display: none;">
+                {factory-1}
+                {factory-2}
+                {factory-3}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="economy-btn" class="collapse-button" onclick="expand('economy')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Economy</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {economy-1}
-            {economy-2}
-            {economy-3}
-            {economy-4}
+            <div id="economy-data" style="display: none;">
+                {economy-1}
+                {economy-2}
+                {economy-3}
+                {economy-4}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="bldg-attack-btn" class="collapse-button" onclick="expand('bldg-attack')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Buildings - Attack</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {attack-1}
-            {attack-2}
-            {attack-3}
-            {attack-4}
+            <div id="bldg-attack-data" style="display: none;">
+                {attack-1}
+                {attack-2}
+                {attack-3}
+                {attack-4}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="bldg-defense-btn" class="collapse-button" onclick="expand('bldg-defense')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Buildings - Defense</h3>
-                </div>            
-            </div> 
-            {defense-2}
-            {defense-3}
+                </div>         
+                <div class="span4"></div>   
+            </div>
+            <div id="bldg-defense-data" style="display: none;"> 
+                {defense-2}
+                {defense-3}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="bldg-sensor-btn" class="collapse-button" onclick="expand('bldg-sensor')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Buildings - Sensor</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {sensor-1}
-            {sensor-2}
-            {sensor-3}
+            <div id="bldg-sensor-data" style="display: none;">
+                {sensor-1}
+                {sensor-2}
+                {sensor-3}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="air-btn" class="collapse-button" onclick="expand('air')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Aircraft</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {aircraft-1}
-            {aircraft-2}
-            {aircraft-3}
-            {aircraft-4}
+            <div id="air-data" style="display: none;">
+                {aircraft-1}
+                {aircraft-2}
+                {aircraft-3}
+                {aircraft-4}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="vehicles-btn" class="collapse-button" onclick="expand('vehicles')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Vehicles</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {vehicle-1}
-            {vehicle-2}
-            {vehicle-3}
-            {vehicle-4}
+            <div id="vehicles-data" style="display: none;">
+                {vehicle-1}
+                {vehicle-2}
+                {vehicle-3}
+                {vehicle-4}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="naval-btn" class="collapse-button" onclick="expand('naval')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Navy</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {naval-1}
-            {naval-2}
-            {naval-3}
-            {naval-4}
+            <div id="naval-data" style="display: none;">
+                {naval-1}
+                {naval-2}
+                {naval-3}
+                {naval-4}
+            </div>
 
-
-            <div class="row-fluid">
-                <div class="category black-bg">
+            <div class="row-fluid category black-bg">
+                <div class="span4 collapse-link-spacing">                    
+                    <button id="support-btn" class="collapse-button" onclick="expand('support')">Expand</button>
+                </div>
+                <div class="span4">
                     <h3 class="electrolize">Support</h3>
-                </div>            
+                </div>         
+                <div class="span4"></div>   
             </div>
-            {support-1}
-            {support-2}
-            {support-4}
-
+            <div id="support-data" style="display: none;">
+                {support-1}
+                {support-2}
+                {support-4}
+            </div>
 
         </div>
     </div>        
--- assets/js/faf_unit_db.js
+++ assets/js/faf_unit_db.js
@@ -1,107 +1,124 @@
-   
-    var $globals = { unit1_race:1, unit1_tier:1, unit2_race:1, unit2_tier:1, unit1_type:'unit', unit2_type:'unit' };
-    var $races = { aeon:1, cybran:2, uef:3, seraphim:4 }; 
-    var $chosens = { unit1:false, unit2:false };
-    
-    
-    function changeRace($unit, $race) {
-        clearRace($unit);        
-        document.getElementById($unit + "_" + $race).setAttribute("class", "compare-selected");   
-        $globals[$unit + "_race"] = $races[$race];             
-        showStep2($unit);
-        clearSelect($unit);
-        populateSelect($unit);
-        changeBorder($unit, $race);
-    }
-        
-    function changeTier($unit, $tier) {
-        clearTier($unit);        
-        document.getElementById($unit + "_tier" + $tier).setAttribute("class", "compare-selected");
-        $globals[$unit + "_tier"] = $tier;
-        showStep3($unit);
-        clearSelect($unit);
-        populateSelect($unit);
-    }
-    
-    function changeType($unit, $type) {
-        clearType($unit);
-        document.getElementById($unit + "_type_" + $type).setAttribute("class", "compare-selected");
-        $globals[$unit + "_type"] = $type;
-        clearSelect($unit);
-        showStep4($unit);
-        populateSelect($unit);        
-    }
-    
-    function changeBorder($unit, $race) {       
-        if($race == 'aeon') {
-            document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(10, 157, 47, .30);");
-        } else if ($race == 'cybran') {
-            document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(223, 45, 14, .30);");
-        } else if ($race == 'uef') {
-            document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(45, 120, 178, .30);");
-        } else if ($race == 'seraphim') {
-            document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(241, 194, 64, .30);");
-        }
 
-    }
-    
-    function showStep2($unit) {
-        document.getElementById($unit + "_step2").setAttribute("style", "visibility: visible;");
-    }
-    
-    function showStep3($unit) {
-        document.getElementById($unit + "_step3").setAttribute("style", "visibility: visible;");
-    }
-    
-    function showStep4($unit) {
-        document.getElementById($unit + "_step4").setAttribute("style", "visibility: visible;");
-        $chosens[$unit] = true;
-    }
-    
-    function showStep5() {
-        document.getElementById("step5").setAttribute("style", "visibility: visible;");
-    }
-    
-    function test() {
-        alert("unit 1 race: " + $globals["unit1_race"]
-                + "\nunit 1 tier: " + $globals["unit1_tier"]
-                + "\nunit 2 race: " + $globals["unit2_race"]
-                + "\nunit 2 tier: " + $globals["unit2_tier"]);
-    }  
-    
-    function populateSelect($unit) {
-        clearSelect($unit);
-        $.ajax({ url: "compare/getOptions/" + $globals[$unit + "_race"] + "/" + $globals[$unit + "_tier"] + "/" + $globals[$unit + "_type"] })
-        .done(function( html ) { $( "#" + $unit + "_select" ).append( html ); });
+var $globals = { unit1_race:1, unit1_tier:1, unit2_race:1, unit2_tier:1, unit1_type:'unit', unit2_type:'unit' };
+var $races = { aeon:1, cybran:2, uef:3, seraphim:4 }; 
+var $chosens = { unit1:false, unit2:false };
 
-        if($chosens["unit1"] && $chosens["unit2"]) {
-            showStep5();
-        }        
-    }
+
+function changeRace($unit, $race) {
+    clearRace($unit);        
+    document.getElementById($unit + "_" + $race).setAttribute("class", "compare-selected");   
+    $globals[$unit + "_race"] = $races[$race];             
+    showStep2($unit);
+    clearSelect($unit);
+    populateSelect($unit);
+    changeBorder($unit, $race);
+}
     
-    function clearRace($unit) {
-        document.getElementById($unit + "_aeon").setAttribute("class", "compare-unselected");
-        document.getElementById($unit + "_cybran").setAttribute("class", "compare-unselected");
-        document.getElementById($unit + "_uef").setAttribute("class", "compare-unselected");
-        document.getElementById($unit + "_seraphim").setAttribute("class", "compare-unselected");
+function changeTier($unit, $tier) {
+    clearTier($unit);        
+    document.getElementById($unit + "_tier" + $tier).setAttribute("class", "compare-selected");
+    $globals[$unit + "_tier"] = $tier;
+    showStep3($unit);
+    clearSelect($unit);
+    populateSelect($unit);
+}
+
+function changeType($unit, $type) {
+    clearType($unit);
+    document.getElementById($unit + "_type_" + $type).setAttribute("class", "compare-selected");
+    $globals[$unit + "_type"] = $type;
+    clearSelect($unit);
+    showStep4($unit);
+    populateSelect($unit);        
+}
+
+function changeBorder($unit, $race) {       
+    if($race == 'aeon') {
+        document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(10, 157, 47, .30);");
+    } else if ($race == 'cybran') {
+        document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(223, 45, 14, .30);");
+    } else if ($race == 'uef') {
+        document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(45, 120, 178, .30);");
+    } else if ($race == 'seraphim') {
+        document.getElementById($unit + "_wrapper").setAttribute("style", "background: rgba(241, 194, 64, .30);");
     }
+
+}
+
+function showStep2($unit) {
+    document.getElementById($unit + "_step2").setAttribute("style", "visibility: visible;");
+}
+
+function showStep3($unit) {
+    document.getElementById($unit + "_step3").setAttribute("style", "visibility: visible;");
+}
+
+function showStep4($unit) {
+    document.getElementById($unit + "_step4").setAttribute("style", "visibility: visible;");
+    $chosens[$unit] = true;
+}
+
+function showStep5() {
+    document.getElementById("step5").setAttribute("style", "visibility: visible;");
+}
+
+function test() {
+    alert("unit 1 race: " + $globals["unit1_race"]
+            + "\nunit 1 tier: " + $globals["unit1_tier"]
+            + "\nunit 2 race: " + $globals["unit2_race"]
+            + "\nunit 2 tier: " + $globals["unit2_tier"]);
+}  
+
+function populateSelect($unit) {
+    clearSelect($unit);
+    $.ajax({ url: "compare/getOptions/" + $globals[$unit + "_race"] + "/" + $globals[$unit + "_tier"] + "/" + $globals[$unit + "_type"] })
+    .done(function( html ) { $( "#" + $unit + "_select" ).append( html ); });
+
+    if($chosens["unit1"] && $chosens["unit2"]) {
+        showStep5();
+    }        
+}
+
+function clearRace($unit) {
+    document.getElementById($unit + "_aeon").setAttribute("class", "compare-unselected");
+    document.getElementById($unit + "_cybran").setAttribute("class", "compare-unselected");
+    document.getElementById($unit + "_uef").setAttribute("class", "compare-unselected");
+    document.getElementById($unit + "_seraphim").setAttribute("class", "compare-unselected");
+}
+
+function clearTier($unit) {
+    document.getElementById($unit + "_tier1").setAttribute("class", "compare-unselected");
+    document.getElementById($unit + "_tier2").setAttribute("class", "compare-unselected");
+    document.getElementById($unit + "_tier3").setAttribute("class", "compare-unselected");
+    document.getElementById($unit + "_tier4").setAttribute("class", "compare-unselected");
+}
+
+function clearType($unit) {
+    document.getElementById($unit + "_type_unit").setAttribute("class", "compare-unselected");
+    document.getElementById($unit + "_type_building").setAttribute("class", "compare-unselected");
+}
+
+function clearSelect($unit) {
+    document.getElementById($unit + "_select").innerHTML = "";
+}
+
+function hideCompareButton() {
     
-    function clearTier($unit) {
-        document.getElementById($unit + "_tier1").setAttribute("class", "compare-unselected");
-        document.getElementById($unit + "_tier2").setAttribute("class", "compare-unselected");
-        document.getElementById($unit + "_tier3").setAttribute("class", "compare-unselected");
-        document.getElementById($unit + "_tier4").setAttribute("class", "compare-unselected");
-    }
-    
-    function clearType($unit) {
-        document.getElementById($unit + "_type_unit").setAttribute("class", "compare-unselected");
-        document.getElementById($unit + "_type_building").setAttribute("class", "compare-unselected");
-    }
-    
-    function clearSelect($unit) {
-        document.getElementById($unit + "_select").innerHTML = "";
-    }
-    
-    function hideCompareButton() {
-        
-    }
+}
+
+function collapse($category) {
+    document.getElementById($category + "-btn").innerHTML = "+";
+    var $newfunc = "expand('" + $category + "')";
+    document.getElementById($category + "-btn").setAttribute("onclick", $newfunc);
+    document.getElementById($category + "-data").setAttribute("style", "display: none;");
+}
+
+function expand($category) {
+    document.getElementById($category + "-btn").innerHTML = "-";
+    var $newfunc = "collapse('" + $category + "')";
+    document.getElementById($category + "-btn").setAttribute("onclick", $newfunc);    
+    document.getElementById($category + "-data").setAttribute("style", " ");
+}
+
+
+
